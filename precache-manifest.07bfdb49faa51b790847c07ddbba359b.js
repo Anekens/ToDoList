a613a8992470295e838eba3f7526abb25d65a17c
Pavel Milenki <p.milenkii@gmail.com>
2019-12-12 16:27:32 +0300

    Updates

---
+++ precache-manifest.07bfdb49faa51b790847c07ddbba359b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cad56641e7b9d73e628fec9d57464ac",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "17777ce4a93dd6694535",
+    "url": "/ToDoList/static/css/main.c507134e.chunk.css"
+  },
+  {
+    "revision": "be1463f5b4a99a3443d7",
+    "url": "/ToDoList/static/js/2.ac473dcf.chunk.js"
+  },
+  {
+    "revision": "17777ce4a93dd6694535",
+    "url": "/ToDoList/static/js/main.e5842a52.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  }
+]);
